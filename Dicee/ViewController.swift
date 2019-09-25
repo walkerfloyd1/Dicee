90e985c6640aee42cc1cc75871a74f908d6b53b9
Walker Floyd <walkerkeatley@yahoo.com>
2019-09-24 17:09:39 -0700

    ints

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
 
     @IBOutlet weak var diceImageView1: UIImageView!
     
@@ -19,7 +22,10 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex1 = Int (arc4random_uniform(6))
+        randomDiceIndex2 = Int (arc4random_uniform(6))
         
+        print(randomDiceIndex1)
     }
     
 }
