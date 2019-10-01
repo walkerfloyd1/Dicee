38120861c6bb8cd0a30d56855f96752198f5cfc0
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-01 15:54:18 -0700

    show random numbers on roll

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+
 
     @IBOutlet weak var diceImageView1: UIImageView!
     
@@ -26,6 +29,10 @@ class ViewController: UIViewController {
         randomDiceIndex2 = Int (arc4random_uniform(6))
         
         print(randomDiceIndex1)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
