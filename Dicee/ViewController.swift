e2051971d72a51b839c2af3ce6d3b4ceaa0f1ea2
Walker Floyd <walkerkeatley@yahoo.com>
2019-10-01 16:57:31 -0700

    shake gesture response

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -17,8 +17,8 @@ class ViewController: UIViewController {
 
 
     @IBOutlet weak var diceImageView1: UIImageView!
-    
     @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateDiceImages()
@@ -34,6 +34,10 @@ class ViewController: UIViewController {
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
     }
     
 }
